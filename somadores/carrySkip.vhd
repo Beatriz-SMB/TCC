df70321a526d6565072f384bd4c842066695ac32
Beatriz-SMB <bsnbernardors@gmail.com>
2026-01-05 21:17:28 -0300

    feat: refactor carrySkip architecture for improved signal handling and clarity

--- somadores/carrySkip.vhd
+++ somadores/carrySkip.vhd
@@ -18,7 +18,7 @@ end carrySkip;
 architecture behav of carrySkip is
     -- Full Adder --------------------
     component fullAdder is
-        Port ( 
+        port ( 
             A, B, C_in : in std_logic;
             Sum, C_out : out std_logic
         );
@@ -26,15 +26,16 @@ architecture behav of carrySkip is
 
     -- Mux --------------------------
     component mux is
-        Port ( 
-            A, B, S : in std_logic;
+        port ( 
+            A, B : in std_logic;
+            S : in std_logic;
             mux_out : out std_logic
         );
     end component;
 
     -- intermediate signal declaration
-    signal C0, C1, C2, C3: std_logic;
-    signal S0, S1, S2, S3 : std_logic;
+    signal C : std_logic_vector(3 downto 0);
+
     signal P, P0, P1, P2, P3 : std_logic;
 
 begin
@@ -43,8 +44,8 @@ begin
         A => A_IN(0),
         B => B_IN(0),
         C_in => C_IN,
-        C_out => C0,
-        Sum => S0
+        C_out => C(0),
+        Sum => SOMA_OUT(0)
     );
 
     P0 <= A_IN(0) xor B_IN(0);
@@ -52,9 +53,9 @@ begin
     x2: fullAdder port map (
         A => A_IN(1),
         B => B_IN(1),
-        C_in => C0,
-        C_out => C1,
-        Sum => S1
+        C_in => C(0),
+        C_out => C(1),
+        Sum => SOMA_OUT(1)
     );
 
     P1 <= A_IN(1) xor B_IN(1);
@@ -62,9 +63,9 @@ begin
     x3: fullAdder port map (
         A => A_IN(2),
         B => B_IN(2),
-        C_in => C1,
-        C_out => C2,
-        Sum => S2
+        C_in => C(1),
+        C_out => C(2),
+        Sum => SOMA_OUT(2)
     );
 
     P2 <= A_IN(2) xor B_IN(2);
@@ -72,9 +73,9 @@ begin
     x4_zero: fullAdder port map (
         A => A_IN(3),
         B => B_IN(3),
-        C_in => C2,
-        C_out => C3,
-        Sum => S3
+        C_in => C(2),
+        C_out => C(3),
+        Sum => SOMA_OUT(3)
     );
 
     P3 <= A_IN(3) xor B_IN(3);
@@ -82,8 +83,8 @@ begin
     P <= P0 and P1 and P2 and P3;
 
     m: mux port map (
-        A => C_IN,
-        B => C3,
+        A => C(3),
+        B => C_IN,
         S => P,
         mux_out => C_OUT
     );
