d43b10ee2f995cc7927fa8f92012e5f32a2bcc09
Beatriz-SMB <bsnbernardors@gmail.com>
2026-01-07 13:11:21 -0300

    feat: update carrySelect entity to use std_logic_vector for mux inputs and outputs

--- somadores/carrySelect.vhd
+++ somadores/carrySelect.vhd
@@ -6,11 +6,11 @@ use IEEE.NUMERIC_STD.ALL;
 -- entity
 entity carrySelect is
     Port ( 
-        A_IN : in std_logic_vector(3 downto 0);
-        B_IN : in std_logic_vector(3 downto 0);
-        C_IN : in std_logic_vector(0 downto 0);
-        SOMA_OUT : out std_logic_vector(3 downto 0);
-        C_OUT : out std_logic_vector(0 downto 0)
+        A_IN        : in std_logic_vector(3 downto 0);
+        B_IN        : in std_logic_vector(3 downto 0);
+        C_IN        : in std_logic_vector(0 downto 0);
+        SOMA_OUT    : out std_logic_vector(3 downto 0);
+        C_OUT       : out std_logic_vector(0 downto 0)
     );
 end carrySelect;
 
@@ -26,9 +26,11 @@ architecture behav of carrySelect is
 
     -- Mux --------------------------
     component mux is
+        generic ( n: positive := 1);
         Port ( 
-            A, B, S : in std_logic;
-            mux_out : out std_logic
+            A, B : in std_logic_vector(n-1 downto 0); 
+            S : in std_logic;
+            mux_out : out std_logic_vector(n-1 downto 0)
         );
     end component;
 
@@ -36,6 +38,8 @@ architecture behav of carrySelect is
     signal C : std_logic_vector(7 downto 0);
 
     signal A0, A1, A2, A3, B0, B1, B2, B3 : std_logic;
+    signal s_A : std_logic_vector(3 downto 0);
+    signal s_B : std_logic_vector(3 downto 0);
 
     signal zero : std_logic := '0';
 	signal one  : std_logic := '1';
@@ -46,7 +50,7 @@ begin
         B => B_IN(0),
         C_in => zero,
         C_out => C(0),
-        Sum => A0
+        Sum => s_A(0)
     );
 
     -- C_in = 1
@@ -55,14 +59,14 @@ begin
         B => B_IN(0),
         C_in => one,
         C_out => C(1),
-        Sum => B0
+        Sum => s_B(0)
     );
 
-    m1: mux port map (
-        A => A0,
-        B => B0,
+    m1: mux generic map(1) port map (
+        A => s_A(0 downto 0),
+        B => s_B(0 downto 0),
         S => C_IN(0),
-        mux_out => SOMA_OUT(0)
+        mux_out => SOMA_OUT(0 downto 0)
     );
 
     -- C_in = 0
@@ -71,7 +75,7 @@ begin
         B => B_IN(1),
         C_in => C(0),
         C_out => C(2),
-        Sum => A1
+        Sum => s_A(1)
     );
 
     -- C_in = 1
@@ -80,14 +84,14 @@ begin
         B => B_IN(1),
         C_in => C(1),
         C_out => C(3),
-        Sum => B1
+        Sum => s_B(1)
     );
 
-    m2: mux port map (
-        A => A1,
-        B => B1,
+    m2: mux generic map(1) port map (
+        A => s_A(1 downto 1),
+        B => s_B(1 downto 1),
         S => C_IN(0),
-        mux_out => SOMA_OUT(1)
+        mux_out => SOMA_OUT(1 downto 1)
     );
 
     -- C_in = 0
@@ -96,7 +100,7 @@ begin
         B => B_IN(2),
         C_in => C(2),
         C_out => C(4),
-        Sum => A2
+        Sum => s_A(2)
     );
 
     -- C_in = 1
@@ -105,14 +109,14 @@ begin
         B => B_IN(2),
         C_in => C(3),
         C_out => C(5),
-        Sum => B2
+        Sum => s_B(2)
     );
 
-    m3: mux port map (
-        A => A2,
-        B => B2,
+    m3: mux generic map(1) port map (
+        A => s_A(2 downto 2),
+        B => s_B(2 downto 2),
         S => C_IN(0),
-        mux_out => SOMA_OUT(2)
+        mux_out => SOMA_OUT(2 downto 2)
     );
 
     -- C_in = 0
@@ -121,7 +125,7 @@ begin
         B => B_IN(3),
         C_in => C(4),
         C_out => C(6),
-        Sum => A3
+        Sum => s_A(3)
     );
 
     -- C_in = 1
@@ -130,21 +134,21 @@ begin
         B => B_IN(3),
         C_in => C(5),
         C_out => C(7),
-        Sum => B3
+        Sum => s_B(3)
     );
 
-    m4: mux port map (
-        A => A3,
-        B => B3,
+    m4: mux generic map(1) port map (
+        A => s_A(3 downto 3),
+        B => s_B(3 downto 3),
         S => C_IN(0),
-        mux_out => SOMA_OUT(3)
+        mux_out => SOMA_OUT(3 downto 3)
     );
 
-    mux_Cout: mux port map (
-        A => C(6),
-        B => C(7),
+    mux_Cout: mux generic map(1) port map (
+        A => C(6 downto 6),
+        B => C(7 downto 7),
         S => C_IN(0),
-        mux_out => C_OUT(0)
+        mux_out => C_OUT(0 downto 0)
     );
     
 end behav;
