f22ddcd4c538f194473a0d36dda5ab58a61ae43c
Beatriz-SMB <bsnbernardors@gmail.com>
2026-01-08 16:26:33 -0300

    feat: modify flipflopD entity to simplify input and output types

--- somadores/flipflop.vhd
+++ somadores/flipflop.vhd
@@ -5,11 +5,10 @@ use IEEE.NUMERIC_STD.ALL;
 
 -- entity
 entity flipflopD is
-	generic ( n: positive);
     port(
-			D   : in std_logic_vector(n-1 downto 0);
+			D   : in std_logic;
 			CLK, RESET : in std_logic;
-			Q   : out std_logic_vector(n-1 downto 0)
+			Q   : out std_logic
     );
 end flipflopD;
 
@@ -21,7 +20,7 @@ begin
 	process(CLK)
 	begin
 		if RESET = '1' then
-			Q <= (others => '0');
+			Q <= '0';
 		elsif rising_edge(CLK) then
 			Q <= D;
 		end if;
